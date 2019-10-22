526c07887932738169cc783be101f1b7b1a08cdf
blizzardnya <e.hmelnikov@yandex.by>
2019-10-22 14:37:52 +0300

    22.10.2019

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.urls import reverse
+
 from accounts.models import ShopUser
 from bid.models import Product, Unit
 
 
 class Order(models.Model):
-    """ Модель заказа """
+    """ Модель заявки """
     user = models.ForeignKey(ShopUser, verbose_name="Пользователь", on_delete=models.CASCADE)
     created = models.DateTimeField("Дата создания", auto_now_add=True)
     assembled = models.DateTimeField("Дата комплектовки", null=True, blank=True)
@@ -33,8 +35,8 @@ class Order(models.Model):
 
     class Meta:
         ordering = ('-created', )
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
 
     def get_total_cost(self):
         return round(sum(item.get_cost() for item in self.items.all()), 2)
@@ -46,22 +48,25 @@ class Order(models.Model):
     def get_status_color(self):
         return self.STATUS_COLORS.get(self.status)
 
+    def get_absolute_url(self):
+        return reverse('orders:view_order', args=[self.id])
+
     def __str__(self):
-        return 'Order {}'.format(self.id)
+        return 'Заявка {}'.format(self.id)
 
     get_total_cost.short_description = 'Итого'
 
 
 class OrderItem(models.Model):
-    """ Модель строк заказа """
-    order = models.ForeignKey(Order, verbose_name="Заказ", related_name='items', on_delete=models.CASCADE)
+    """ Модель строк заявки """
+    order = models.ForeignKey(Order, verbose_name="Заявка", related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, verbose_name="Товар", related_name='order_item', on_delete=models.CASCADE)
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
     quantity = models.DecimalField("Количество", max_digits=10, decimal_places=2)
 
     class Meta:
-        verbose_name = 'Строка заказа'
-        verbose_name_plural = 'Строки заказа'
+        verbose_name = 'Строка заявки'
+        verbose_name_plural = 'Строки заявки'
 
     def get_cost(self):
         return round(self.price * self.quantity, 2)
